f7b9153ab59a176703e89de71f0ab93e21d026da
OmarDriouch <omardriouch29@gmail.com>
2023-05-02 16:31:03 +0100

    adding the authentication with the firebase

--- public/Js/script.js
+++ public/Js/script.js
@@ -1,19 +1,38 @@
-var bgImages = [
-    'D:/omar\'s PC/omar pc/2000-4K-Wallpapers-Themefoxx-01/image1.jpg',
-    'D:/omar\'s PC/omar pc/2000-4K-Wallpapers-Themefoxx-01/image2.jpg',
-    'D:/omar\'s PC/omar pc/2000-4K-Wallpapers-Themefoxx-01/image3.jpg'
-  ];
-  
-  var currentIndex = 0;
-  var bgImage = document.querySelector('.bgimage');
-  
-  function changeBgImage() {
-    bgImage.style.backgroundImage = 'url(' + bgImages[currentIndex] + ')';
-    currentIndex++;
-    if (currentIndex === bgImages.length) {
-      currentIndex = 0;
-    }
-  }
-  
-  setInterval(changeBgImage, 5000);
-  
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyCIw0v4g7IsQe-12a6RJR9Sd8kjF3zUfto",
+  authDomain: "wallpaperapp-b16b9.firebaseapp.com",
+  projectId: "wallpaperapp-b16b9",
+  storageBucket: "wallpaperapp-b16b9.appspot.com",
+  messagingSenderId: "714925262199",
+  appId: "1:714925262199:web:9072e688a72ffdf91bcd72",
+  measurementId: "G-F8W80P3CRW"
+};
+
+firebase.initializeApp(firebaseConfig);
+$("#btnlogout").click(function() {
+   
+  alert(" fewfe ")
+});
+
+
+$("#btnprimary").click(function() {
+  var email = $("#email").val();
+  var password = $("#password").val();
+
+  firebase.auth().signInWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      // Signed in
+      var user = userCredential.user;
+      // Redirect to another page or update UI
+      window.location.href = "admin.html";
+      
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+       
+    });
+});
+
+
